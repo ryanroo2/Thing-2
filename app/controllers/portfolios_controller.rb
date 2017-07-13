95ba4da764c0d8bff458047d526f449a74bcf7b4
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:02:29 +0000

    Things

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy, :toggle_status]
   layout 'portfolio'
   def index
     @portfolio_items = Portfolio.all
@@ -28,14 +29,12 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
     if @portfolio_item.technologies.length == 0
       3.times { @portfolio_item.technologies.build }
     end
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -47,12 +46,10 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
     # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
 
     # Destroy/delete the record
     @portfolio_item.destroy
@@ -72,5 +69,9 @@ class PortfoliosController < ApplicationController
                                       technologies_attributes: [:name]
                                       )
   end  
+  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.fine(params[:id])
+  end
 
 end
