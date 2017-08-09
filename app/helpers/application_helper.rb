b34078a3d90091d0748db62fd77824020e43e738
Jordan Hudgens <jordan@devcamp.com>
2017-08-09 15:24:10 +0000

    Updated many many things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,25 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-     (link_to "Register", new_user_registration_path, class: style) +
-     " ".html_safe +
-     (link_to "Login", new_user_session_path, class: style)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
-  end  
-  def source_helper(layout_name)
+  end
+
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
-  
+
   def copyright_generator
-    CopyrightSigil::Renderer.copyright 'Ryan Peterson', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Ryan Peterson', 'All rights reserved'
   end
-  
+
   def nav_items
     [
       {
@@ -27,7 +28,7 @@ module ApplicationHelper
       },
       {
         url: about_me_path,
-        title: 'About'
+        title: 'About Me'
       },
       {
         url: contact_path,
@@ -40,33 +41,38 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-      }
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
     ]
   end
-  
-  def nav_helper(style, tag_type)
+
+  def nav_helper style, tag_type
     nav_links = ''
-    
+
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
+
     nav_links.html_safe
   end
-  
+
   def active? path
     "active" if current_page? path
   end
-  
+
   def alerts
     alert = (flash[:alert] || flash[:error] || flash[:notice])
-    
+
     if alert
-      alert_generator(alert)
+      alert_generator alert
     end
   end
-  
+
   def alert_generator msg
-    js add_gritter(msg, :title => "Current Page", sticky:false)
+    js add_gritter(msg, title: "Ryan Peterson Portfolio", sticky: false)
   end
-  
+
 end
